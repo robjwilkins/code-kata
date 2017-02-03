03550ccbf0820eaba899da902d337fa4437b20bf
Robin Wilkins <robjwilkins@gmail.com>
2017-02-03 21:15:17 +0000

    fix some problems with attempt 2

--- binary-chop/attempt-two/chop-two.js
+++ binary-chop/attempt-two/chop-two.js
@@ -1,24 +1,23 @@
-exports.indexOf = function(list, target) {
-    var size = list.length;
-    var index = size / 2;
-    while (true) {
+indexOf = function(list, target) {
+    var size = list.length,
+        index = size / 2,
+        lowerBound = 0,
+        upperBound = size -1;
+
+    while (lowerBound <= upperBound) {
+        index = (lowerBound + upperBound) / 2 | 0;
         var candidate = list[index];
-        if (candidate > target) {
-            if (index === 1) {
-                if (target != list[0]){
-                    return -1;
-                } else {
-                    return 0;
-                }
-            }
-            index = Math.round(index / 2);
-        } else if (candidate < target) {
-            index = Math.round(index + ((size-index)/2));
-            if (index === size){
-                return -1;
-            }
+        if (candidate < target) {
+            lowerBound = index + 1;
+        } else if (candidate > target) {
+            upperBound = index - 1;
         } else {
             return index;
         }
     }
+    return -1;
 }
+
+exports.indexOf = indexOf;
+var theList = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+console.log(indexOf(theList, 1));
--- binary-chop/test/test-chop.js
+++ binary-chop/test/test-chop.js
@@ -8,7 +8,7 @@ const
     chopperTwo = require('./../attempt-two/chop-two.js');
 
 exports.testChopperOne = function(test) {
-    test.expect(7);
+    test.expect(8);
     test.equal(chopperOne.indexOf(theList, 15), 14);
     test.equal(chopperOne.indexOf(theList, 1), 0);
     test.equal(chopperOne.indexOf(theList, 2), 1);
@@ -16,11 +16,12 @@ exports.testChopperOne = function(test) {
     test.equal(chopperOne.indexOf(theList, 19), 18);
     test.equal(chopperOne.indexOf(theList, 20), 19);
     test.equal(chopperOne.indexOf(theList, 21), -1);
+    test.equal(chopperTwo.indexOf(theList, 12), 11);
     test.done();
 }
 
 exports.testChopperTwo = function(test) {
-    test.expect(7);
+    //test.expect(8);
     test.equal(chopperTwo.indexOf(theList, 15), 14);
     test.equal(chopperTwo.indexOf(theList, 1), 0);
     test.equal(chopperTwo.indexOf(theList, 2), 1);
@@ -28,5 +29,7 @@ exports.testChopperTwo = function(test) {
     test.equal(chopperTwo.indexOf(theList, 19), 18);
     test.equal(chopperTwo.indexOf(theList, 20), 19);
     test.equal(chopperTwo.indexOf(theList, 21), -1);
+    test.equal(chopperTwo.indexOf(theList, 12), 11);
+    test.equal(chopperTwo.indexOf(theList, 9), 8);
     test.done();
 }
